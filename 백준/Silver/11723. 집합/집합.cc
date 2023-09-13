a276c4ac08da0e21544e5eb3dfc998b336b50b01
김덕륜 <96904134+Kimdeokryun@users.noreply.github.com>
2023-09-13 16:59:54 +0900

    [Silver V] Title: 집합, Time: 580 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -1,59 +1,61 @@
 #include <iostream>
-#include <cstring>
- 
 using namespace std;
- 
- 
-//비트마스크로 풀기
- 
-int main(){
-    
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    
-    string str = "";
-    int M, x;
-    
-    int S = 0;
-   
-    cin >> M;
-    for(int i = 0; i<M; i++){
-        cin >> str;
-        
-        if(str=="add"){
-            cin >> x;
-            S |= (1<<x);
-        }
-        
-        else if(str=="remove"){
-            cin >> x;
-            S &= ~(1<<x);
-        }
-        else if(str=="check"){
-            cin >> x;
-            if(S & (1<<x)){   //있으면
-                cout << "1\n";
-            } else{
-                cout << "0\n";
-            }
-        }
-        else if(str=="toggle"){
-           cin >> x;
-            if(S & (1<<x)){  //있으면
-                S &= ~(1<<x); //없애고
-            } else{             //없으면
-                S |= (1<<x);     //추가
-            }
-        }
-        else if(str=="all"){
-            S = (1<<21) - 1;
-        }
-        else if(str=="empty"){
-            S = 0;
-        }
-        
-    
-    }
-    
-    return 0;
+
+// 비트마스크
+/*
+And 연산 (&): 대응하는 두 비트가 모두 1이면 1 반환
+OR 연산 ( | ): 대응하는 두 비트 중 하나라도 1이면 1 반환
+Shift 연산 («,») : 비트를 왼쪽 또는 오른쪽으로 한칸씩 이동
+XOR 연산 (^): 대응하는 두 비트가 다르면 1 같으면 0 반환
+Not 연산 (~) : 비트를 반전
+
+
+add -> |= (1 << val)
+remove -> &= ~(1 << val)
+check -> &= (1 << val)
+toggle -> ^= (1 << val)
+*/
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int M;
+	cin >> M;
+
+	string command;
+	int num, bit = 0;
+
+	for (int i = 0; i < M; i++) 
+	{
+		cin >> command;
+		if (command == "add")
+		{
+			cin >> num;
+			// or 연산자를 통해 val번째 자리수를 1로 채운다.
+			bit |= (1 << num);
+		}
+		else if (command == "remove")
+		{
+			cin >> num;
+			// ex) 1000 & ~(1000) = 0000
+			bit &= ~(1 << num);
+		}
+		else if (command == "check")
+		{
+			cin >> num;
+			if (bit & (1 << num)) { cout << "1\n"; }
+			else { cout << "0\n"; }
+		}
+		else if (command == "toggle")
+		{
+			cin >> num;
+			// BIT의 val번째 자리가 1이라면 1^1=0이 되어 1->0이 되고
+			// val번째 자리가 0이라면 0^1=1 이 되어 0 -> 1이 된다.
+			bit ^= (1 << num);
+		}
+		else if (command == "all") { bit = (1 << 21) - 1; }
+		else if (command == "empty") { bit = 0; }
+	}
+	return 0;
 }
