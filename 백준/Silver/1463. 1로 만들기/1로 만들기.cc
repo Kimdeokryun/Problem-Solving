46ecd48da6ff417be48ea2ed39ac3230a5fef146
김덕륜 <96904134+Kimdeokryun@users.noreply.github.com>
2023-09-15 15:31:02 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
 using namespace std; 
@@ -11,30 +12,20 @@ int main() {
 	cout.tie(NULL);
 
 	int N;
-	
 	cin >> N;
 
 	dp[1] = 0;
-	dp[2] = 1;
-	dp[3] = 1;
 
-	for (int i = 4; i <= N; i++) 
+	for (int i = 2; i <= N; i++) 
 	{
-		if (i % 2 == 0 && i % 3 == 0) 
+		dp[i] = dp[i - 1] + 1;
+		if (i % 2 == 0)
 		{
-			dp[i] = min(dp[i / 2] + 1, dp[i / 3] + 1);
+			dp[i] = min(dp[i / 2] + 1, dp[i]);
 		}
-		else if (i % 2 == 0) 
+		if (i % 3 == 0)
 		{
-			dp[i] = min(dp[i / 2] + 1, dp[i-1] + 1);
-		}
-		else if (i % 3 == 0) 
-		{
-			dp[i] = min(dp[i / 3] + 1, dp[i - 1] + 1);
-		}
-		else
-		{
-			dp[i] = dp[i - 1] + 1;
+			dp[i] = min(dp[i / 3] + 1, dp[i]);
 		}
 	}
 
