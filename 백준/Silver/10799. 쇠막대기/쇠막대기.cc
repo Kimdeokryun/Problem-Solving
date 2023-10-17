f2416d05e5ffe099cadb2e1cb679418b688617f3
김덕륜 <96904134+Kimdeokryun@users.noreply.github.com>
2023-10-17 09:24:59 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -0,0 +1,37 @@
+# include <iostream>
+# include <stack>
+
+using namespace std;
+
+int main() 
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	string input;
+	cin >> input;
+
+	int result = 0;
+	stack<char> str;
+	
+
+	for (int i = 0; i < input.length(); i++)
+	{
+		if (input[i] == '(') 
+		{
+			str.push(input[i]);
+		}
+		else if(input[i] == ')' && input[i-1] == '(')
+		{
+			str.pop();
+			result += str.size();
+		}
+		else {
+			result++;
+			str.pop();
+		}
+	}
+
+	cout << result << endl;
+	return 0;
+}
