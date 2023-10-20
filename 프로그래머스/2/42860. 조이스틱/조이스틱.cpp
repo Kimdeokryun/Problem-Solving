7b5b5f385fced61f8ebcf107686fd9381dedb7f5
김덕륜 <96904134+Kimdeokryun@users.noreply.github.com>
2023-10-20 10:35:25 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int solution(string name) {
+    
+    int answer = 0;
+    int n = name.length();
+    int turn = n - 1; //조이스틱을 한 방향으로 쭉 움직였을 때
+    
+    for (int i = 0; i < n; i++) {
+        if (name[i] - 'A' < 14) answer += name[i] - 'A';
+        else answer += 'Z' - name[i] + 1;
+
+        int ind = i + 1;    // 그 다음 알파벳 이동의 최소를 위해.
+        while (ind < n && name[ind] == 'A') ind++;
+
+        turn = min(turn, i + n - ind + min(i, n - ind));
+    }
+
+    answer += turn;
+    return answer;
+}
