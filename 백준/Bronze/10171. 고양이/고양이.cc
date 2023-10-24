f093e5f16266676503c69ba870dd8d2d5dddc570
김덕륜 <96904134+Kimdeokryun@users.noreply.github.com>
2023-10-24 10:34:02 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main() 
+{
+
+	string str1 = "\\    /\\";
+	string str2 = " )  ( ')";
+	string str3 = "(  /  )";
+	string str4 = " \\(__)|";
+	cout << str1 << "\n";
+	cout << str2 << "\n";
+	cout << str3 << "\n";
+	cout << str4 << "\n";
+	return 0;
+}
