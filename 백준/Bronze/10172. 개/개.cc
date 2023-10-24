3d3e7c7fc8505b80ba75e415618371bffc3cadec
김덕륜 <96904134+Kimdeokryun@users.noreply.github.com>
2023-10-24 10:41:05 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+	string str1 = "|\\_/|";
+	string str2 = "|q p|   /}";
+	string str3 = "( 0 )\"\"\"\\";
+	string str4 = "|\"^\"`    |";
+	string str5 = "||_/=\\\\__|";
+
+	cout << str1 << "\n";
+	cout << str2 << "\n";
+	cout << str3 << "\n";
+	cout << str4 << "\n";
+	cout << str5 << "\n";
+	return 0;
+}
